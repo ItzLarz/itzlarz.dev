c6e3319a057cc24e2ee61c2d3a2a98149c244842
ItzLarz <l.y.kruitwagen@gmail.com>
2023-05-17 23:33:31 +0200

    Added logs for Unhandled Rejections

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const geoip = require("geoip-lite");
 
+// Declaring https server
+let server;
+let serverCrashes = 0;
+
 // Declaring express constants
 const app = express();
 const router = express.Router();
@@ -52,10 +56,7 @@ var useNLOnly = true; // Use NL IP only mode
 var ipBlacklist = [null];
 
 // IP Whitelist for server
-var ipWhitelist = [
-	null,
-	"77.175.92.205"
-];
+var ipWhitelist = [null, "77.175.92.205"];
 
 // Stating which directories to use
 app.use(express.static("scripts"));
@@ -73,115 +74,182 @@ app.use((req, res, next) => {
 
 // Adding the IP-Adress to log and alerting host
 app.use(async (req, res, next) => {
-	var ip = getClientIp(req);
-	if (ip != "77.175.92.205"){
-		if (!ip.startsWith("192.168")){
-			try {
-				var ipFile = fs.openSync("./IP-Log.txt", "r");
-		        var data = fs.readFileSync(ipFile, "utf8");
+  var ip = getClientIp(req);
+  if (ip != "77.175.92.205") {
+    if (!ip.startsWith("192.168")) {
+      try {
+        var ipFile = fs.openSync("./IP-Log.txt", "r");
+        var data = fs.readFileSync(ipFile, "utf8");
 
-		   		if (data.includes(ip)) {
-		   			if (!debugMode) {
-			   			await client.sendMessage(chatId, "Client with ip " + ip + " accessed the server (old connection) (country: " + geoip.lookup(ip).country + ")");
-			   			await client.markChatUnread(chatId);
-		   			}
-		   		}
+        if (data.includes(ip)) {
+          if (!debugMode) {
+            await client.sendMessage(
+              chatId,
+              "Client with ip " +
+                ip +
+                " accessed the server (old connection) (country: " +
+                geoip.lookup(ip).country +
+                ")"
+            );
+            await client.markChatUnread(chatId);
+          }
+        } else {
+          if (!debugMode) {
+            await client.sendMessage(
+              chatId,
+              "Client with ip " +
+                ip +
+                " accessed the server (new connection) (country: " +
+                geoip.lookup(ip).country +
+                ")"
+            );
+            await client.markChatUnread(chatId);
+          }
+        }
 
-				else {
-					if (!debugMode) {
-						await client.sendMessage(chatId, "Client with ip " + ip + " accessed the server (new connection) (country: " + geoip.lookup(ip).country + ")");
-						await client.markChatUnread(chatId);
-					}
-				}
+        fs.closeSync(ipFile);
 
-
-		       	fs.closeSync(ipFile);
-
-		  		ipFile = fs.openSync("./IP-Log.txt", "a");
-		  		fs.appendFileSync(ipFile, ip + " - " + new Date() + " - " + JSON.stringify(geoip.lookup(ip))  + "\n", "utf8");
-			}
-
-			catch(err) {
-				if (!debugMode) {
-					await client.sendMessage(chatId, err);
-					await client.markChatUnread(chatId);
-				}
-
-				else {
-					console.log(err);
-				}
-			}
-
-			finally {
-				if (ipFile !== undefined) {
-					fs.closeSync(ipFile);
-				}
-			}
-		}
-	}
-	return next();
+        ipFile = fs.openSync("./IP-Log.txt", "a");
+        fs.appendFileSync(
+          ipFile,
+          ip +
+            " - " +
+            new Date() +
+            " - " +
+            JSON.stringify(geoip.lookup(ip)) +
+            "\n",
+          "utf8"
+        );
+      } catch (err) {
+        if (!debugMode) {
+          await client.sendMessage(chatId, err);
+          await client.markChatUnread(chatId);
+        } else {
+          console.log(err);
+        }
+      } finally {
+        if (ipFile !== undefined) {
+          fs.closeSync(ipFile);
+        }
+      }
+    }
+  }
+  return next();
 });
 
 // Choosing what to send to client based on IP-Adress
 app.get("/", async (req, res) => {
-	var ip = getClientIp(req);
-	if (useBlacklist) {
-		for (var i = 0; i < ipBlacklist.length; i++){
-			if (!ip.startsWith(ipBlacklist[i])) { continue; }
-			else {
-				if (!debugMode) {
-					await client.sendMessage(chatId, "Client with ip " + ip + " was kicked from the server (country: " + geoip.lookup(ip).country + ")");
-					await client.markChatUnread(chatId);
-				}
-				res.status(403).send("403 Forbidden" + `<br/>` + "IP-Adress on the blacklist" + `<br/>` + new Date());
-				return res.end();
-			}
-		}
-	}
+  var ip = getClientIp(req);
+  if (useBlacklist) {
+    for (var i = 0; i < ipBlacklist.length; i++) {
+      if (!ip.startsWith(ipBlacklist[i])) {
+        continue;
+      } else {
+        if (!debugMode) {
+          await client.sendMessage(
+            chatId,
+            "Client with ip " +
+              ip +
+              " was kicked from the server (country: " +
+              geoip.lookup(ip).country +
+              ")"
+          );
+          await client.markChatUnread(chatId);
+        }
+        res
+          .status(403)
+          .send(
+            "403 Forbidden" +
+              `<br/>` +
+              "IP-Adress on the blacklist" +
+              `<br/>` +
+              new Date()
+          );
+        return res.end();
+      }
+    }
+  }
 
-	if (useWhitelist) {
-		var through = false;
-		for (var i = 0; i < ipWhitelist.length; i++){
-			if (ip.startsWith(ipWhitelist[i])) {
-				through = true;
-				break;
-			}
-		}
+  if (useWhitelist) {
+    var through = false;
+    for (var i = 0; i < ipWhitelist.length; i++) {
+      if (ip.startsWith(ipWhitelist[i])) {
+        through = true;
+        break;
+      }
+    }
 
-		if (!through) {
-			if (!debugMode) {
-				await client.sendMessage(chatId, "Client with ip " + ip + " was kicked from the server (country: " + geoip.lookup(ip).country + ")");
-				await client.markChatUnread(chatId);
-			}
-			res.status(403).send("403 Forbidden" + `<br/>` + "IP-Adress not on the whitelist" + `<br/>` + new Date());
-			return res.end();
-		}
-	}
+    if (!through) {
+      if (!debugMode) {
+        await client.sendMessage(
+          chatId,
+          "Client with ip " +
+            ip +
+            " was kicked from the server (country: " +
+            geoip.lookup(ip).country +
+            ")"
+        );
+        await client.markChatUnread(chatId);
+      }
+      res
+        .status(403)
+        .send(
+          "403 Forbidden" +
+            `<br/>` +
+            "IP-Adress not on the whitelist" +
+            `<br/>` +
+            new Date()
+        );
+      return res.end();
+    }
+  }
 
-	if (useNLOnly) {
-		if (geoip.lookup(ip)){
-			if (geoip.lookup(ip).country != "NL") {
-				if (!debugMode) {
-					await client.sendMessage(chatId, "Client with ip " + ip + " was kicked from the server (country: " + geoip.lookup(ip).country + ")");
-					await client.markChatUnread(chatId);
-				}
-				res.status(403).send("403 Forbidden" + `<br/>` + "Only Dutch IP-Adresses permitted" + `<br/>` + new Date());
-				return res.end();
-			}
-		}
-	}
+  if (useNLOnly) {
+    if (geoip.lookup(ip)) {
+      if (geoip.lookup(ip).country != "NL") {
+        if (!debugMode) {
+          await client.sendMessage(
+            chatId,
+            "Client with ip " +
+              ip +
+              " was kicked from the server (country: " +
+              geoip.lookup(ip).country +
+              ")"
+          );
+          await client.markChatUnread(chatId);
+        }
+        res
+          .status(403)
+          .send(
+            "403 Forbidden" +
+              `<br/>` +
+              "Only Dutch IP-Adresses permitted" +
+              `<br/>` +
+              new Date()
+          );
+        return res.end();
+      }
+    }
+  }
 
-	if (closed) {
-		res.status(403).send("403 Forbidden" + `<br/>` + "Site Closed" + `<br/>` + new Date());
-		if (!debugMode) {
-			await client.sendMessage(chatId, "Client with ip " + ip + " was kicked from the server (country: " + geoip.lookup(ip).country + ")");
-		}
-		return res.end();
-	}
-
-	else {
-		return res.sendFile(path.join(__dirname + "/index.html"));
-	}
+  if (closed) {
+    res
+      .status(403)
+      .send("403 Forbidden" + `<br/>` + "Site Closed" + `<br/>` + new Date());
+    if (!debugMode) {
+      await client.sendMessage(
+        chatId,
+        "Client with ip " +
+          ip +
+          " was kicked from the server (country: " +
+          geoip.lookup(ip).country +
+          ")"
+      );
+    }
+    return res.end();
+  } else {
+    return res.sendFile(path.join(__dirname + "/index.html"));
+  }
 });
 
 // Send wwebjs message if
@@ -304,7 +372,7 @@ if (!debugMode) {
     await client.sendMessage(chatId, "Whatsapp-webjs client is ready");
 
     // Initializing server (with wwebjs running)
-    https.createServer(serverOptions, app).listen(port, async () => {
+    server = https.createServer(serverOptions, app).listen(port, async () => {
       await client.sendMessage(
         chatId,
         `Server listening on port ${port}, with DNS https://itzlarz.dev`
@@ -318,47 +386,96 @@ if (!debugMode) {
 
 if (debugMode) {
   // Initializing server (without wwebjs running)
-  https.createServer(serverOptions, app).listen(port, () => {
+  server = https.createServer(serverOptions, app).listen(port, () => {
     console.log(
       `Server listening on port ${port}, with DNS https://itzlarz.dev`
     );
   });
 }
 
-process.on('uncaughtException', async (err, origin) => {			
-	try {
-		var errorFile = fs.openSync("./Errors.txt","a");
-		fs.appendFileSync(errorFile,
-			new Date() +
-			"\nCaught exception: " + err + 
-			"Exception origin: "+ origin +
-			"\n \n \n \n \n"
-		);
-		
-	}
+process.on("uncaughtException", async (err, origin) => {
+  try {
+    var errorFile = fs.openSync("./Errors.txt", "a");
+    fs.appendFileSync(
+      errorFile,
+      new Date() +
+        "\nCaught exception: " +
+        err +
+        "\nException origin: " +
+        origin +
+        "\n \n \n \n"
+    );
+  } catch {
+    console.log("\nServer has crashed: ");
+    console.log(`Caught exception: ${err}`);
+    console.log(`Exception origin: ${origin}\n`);
+  } finally {
+    if (errorFile !== undefined) {
+      fs.closeSync(errorFile);
+    }
 
-	catch {
-		console.log("\nServer has crashed: ")
-		console.log(`Caught exception: ${err}`)
-		console.log(`Exception origin: ${origin}\n`)	
-	}
+    try {
+      await client.sendMessage(
+        chatId,
+        "Server has crashed.\n" +
+        `Caught exception: ${err}`
+        `Exception origin: ${origin}`
+      );
+    } finally {
+      server.close();
+      server = https.createServer(serverOptions, app).listen(port, async () => {
+        await client.sendMessage(
+          chatId,
+          `Server listening on port ${port}, with DNS https://itzlarz.dev`
+        );
+      });
 
-	finally {
-		if (errorFile !== undefined) {
-			fs.closeSync(errorFile);
-		}
+      serverCrashes++;
+      if (serverCrashes > 15) {
+        process.exit();
+      }
+    }
+  }
+});
 
-		try {
-			await client.sendMessage(chatId, 
-				`Server has crashed.`
-				`Caught exception: ${err}`
-				`Exception origin: ${origin}\n`
-			);
-			
-		}
+process.on("unhandledRejection", async (reason, origin) => {
+  try {
+    var errorFile = fs.openSync("./Errors.txt", "a");
+    fs.appendFileSync(
+      errorFile,
+      new Date() +
+        "\nUnhandled Rejection: " +
+        reason +
+        "\nRejection origin: " +
+        origin +
+        "\n \n \n \n"
+    );
+  } catch {
+    console.log("\nServer Warning");
+    console.log(`Unhandled Rejection: ${reason}`);
+    console.log(`Rejection origin: ${origin}\n`);
+  } finally {
+    if (errorFile !== undefined) {
+      fs.closeSync(errorFile);
+    }
 
-		finally {
-			process.exit();
-		}
-	}
+    try {
+      await client.sendMessage(
+        chatId,
+        "Server Warning.\n" +
+        `Unhandled Rejection: ${reason}`
+        `Rejection origin: ${origin}`
+      );
+    } finally {
+      serverCrashes++;
+      if (serverCrashes > 10) {
+        process.exit();
+      }
+      client.destroy();
+      client.initialize();
+      client.on("ready", async () => {
+        await client.sendMessage(chatId, "Whatsapp-webjs restarted");
+      });
+    }
+  }
 });
